7417210ddf51d868420bd84ae4513d3c63a7d14a
OzgunEfe <ozgunefe.dev@gmail.com>
2024-09-02 20:22:58 +0100

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ozgun Efe on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Ozgun Efe on 02/09/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
