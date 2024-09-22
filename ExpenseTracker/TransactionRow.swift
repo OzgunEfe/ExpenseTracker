ebd789cce6ceb34ee9a861a2d787d660c28b84cf
OzgunEfe <ozgunefe.dev@gmail.com>
2024-09-22 19:59:04 +0100

    Preview data updated

---
+++ ExpenseTracker/TransactionRow.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionRow.swift
+//  ExpenseTracker
+//
+//  Created by Ozgun Efe on 22/09/2024.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionRow()
+}
