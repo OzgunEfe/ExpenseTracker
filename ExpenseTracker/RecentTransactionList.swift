a43a62b334defd8523323722fb62f5a6fcf61427
OzgunEfe <ozgunefe.dev@gmail.com>
2024-09-23 22:31:47 +0100

    Recent Transaction List screen created.

---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Ozgun Efe on 23/09/2024.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentTransactionList()
+}
