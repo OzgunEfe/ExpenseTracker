6d62a90082ec887616d082f6e73b4b9171bd9584
OzgunEfe <ozgunefe.dev@gmail.com>
2024-09-23 22:32:04 +0100

    Recent Transaction List screen created.

--- ExpenseTracker/RecentTransactionList.swift
+++ ExpenseTracker/RecentTransactionList.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct RecentTransactionList: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                // Header title
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                //Header Link
+                NavigationLink {
+                    
+                } label: {
+                    HStack (spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.text)
+                }
+            }
+            .padding(.top)
+        }
+        .padding()
+        .background(Color.systemBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: 10, x:0, y:5)
     }
 }
 
