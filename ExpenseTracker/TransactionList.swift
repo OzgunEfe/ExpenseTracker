7d02501c5ee21416a8012ed63364584c286962eb
OzgunEfe <ozgunefe.dev@gmail.com>
2024-09-29 20:21:31 +0100

    The navigation link to the TransactionList has been activated.

---
+++ ExpenseTracker/TransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Ozgun Efe on 29/09/2024.
+//
+
+import SwiftUI
+
+struct TransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionList()
+}
